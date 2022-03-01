356029da0376c6c96750d33cebfbf9c28e421d0c
ByrdBassPC <byrdbass@gmail.com>
2022-03-01 17:08:29 -0600

    made adjustments to database.js and need to start putting functionality, also made installs of npm packages

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
+const WorkboxPlugin = require('workbox-webpack-plugin')
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
@@ -18,7 +19,39 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: "Werd-to-the-Words"
+      }),
+      new WebpackPwaManifest({
+        filename: 'manifest.json',
+        name: 'Werd-to-the-Words',
+        short_name: 'W2theWords',
+        description: 'A simple text editor!',
+        background_color: '#00ccff',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('./src/images/byrdistheword.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons')
+          }
+        ]
+      }),
+      new WorkboxPlugin.GenerateSW({
+        exclude: [],
+        runtimeCaching: [{
+          urlPattern: ,
+          handler: ,
+          options: {
+            cacheName: ,
+            expiration {
+              maxEntries: 
+            },
+          },
+        }],
+      })
     ],
 
     module: {
